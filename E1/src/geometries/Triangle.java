9db9a0b5b09410ef38c5a9b0284bbf5fb0e817a7
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-03 22:07:45 +0200

    Update Triangle.java

--- E1/src/geometries/Triangle.java
+++ E1/src/geometries/Triangle.java
@@ -8,11 +8,6 @@ public class Triangle extends Polygon {
 
 	public Triangle(Point3D p1, Point3D p2, Point3D p3) {
 		super(p1, p2, p3);
-		double a = p1.distance(p2);
-		double b = p2.distance(p3);
-		double c = p3.distance(p1);
-		if (!(a + b > c) && (b + c > a) && (a + c > b))
-			throw new IllegalArgumentException();
 	}
 
 }
