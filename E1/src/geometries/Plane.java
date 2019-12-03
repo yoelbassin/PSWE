470131ea82023ea51f9cf02d820a9efa2f870231
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-03 22:08:27 +0200

    Update Plane.java

--- E1/src/geometries/Plane.java
+++ E1/src/geometries/Plane.java
@@ -4,24 +4,17 @@ import primitives.*;
 
 public class Plane implements Geometry {
 
-	Point3D _p;
-	Vector _normal;
+	private Point3D _p;
+	private Vector _normal;
 
 	public Plane(Point3D _p, Vector _normal) {
 		this._p = _p;
-		this._normal = _normal.normalize();
+		this._normal = _normal.normalized();
 	}
 
-	public Plane(Point3D _p1, Point3D _p2, Point3D _p3) {
-		double a = _p1.distance(_p2);
-		double b = _p2.distance(_p3);
-		double c = _p3.distance(_p1);
-		if ((a + b > c) && (b + c > a) && (a + c > b)) {
-			this._p = _p1;
-			this._normal = ((_p3.subtract(_p1)).crossProduct(_p2.subtract(_p1))).normalize();
-		} else {
-			throw new IllegalArgumentException("not a plane");
-		}
+	public Plane(Point3D p1, Point3D p2, Point3D p3) {
+		this._p = p1;
+		this._normal = ((p3.subtract(p1)).crossProduct(p2.subtract(p1))).normalize();
 	}
 
 	public Vector getNormal(Point3D p) {
