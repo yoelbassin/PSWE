ea3fba58c8012f73ce7fbea6c2ae7a2c78d0c4ce
asafJct <44846049+asafJct@users.noreply.github.com>
2019-12-09 19:45:44 +0200

    Update Ray.java

--- E1/src/primitives/Ray.java
+++ E1/src/primitives/Ray.java
@@ -1,15 +1,19 @@
 package primitives;
-
+/**
+ * Class represents 3D space ray.
+ * @author bassi
+ * @author asaf0
+ */
 public class Ray {
 	private Vector dir;
 	private Point3D p;
 
 	// ***************** Constructors ********************** //
 	/*
+	 * Construct a ray with a initial point and direction vector.
+	 * @param vec of direction .
 	 * 
-	 * @param vec , direction Vector.
-	 * 
-	 * @param p , a point on the Ray
+	 * @param p initial point 
 	 */
 	public Ray(Point3D p, Vector vec) {
 
@@ -18,10 +22,191 @@ public class Ray {
 	}
 
 	// ***************** Getters ********************** //
+	/**
+	 * Getter of direction vector
+	 * @return direction vector
+	 */
 	public Vector getDir() {
 		return dir;
 	}
+	/**
+	 * Getter of initial point
+	 * @return initial point
+	 */
+	public Point3D getBasePoint() {
+		return p;
+	}
 
+	// ***************** Administration ******************** //
+	/**
+	 * equal function
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (p.equals(other)) {
+			if (dir.equals(other)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "from " + p + " to " + dir;
+	}
+}
+package primitives;
+/**
+ * Class represents 3D space ray.
+ * @author bassi
+ * @author asaf0
+ */
+public class Ray {
+	private Vector dir;
+	private Point3D p;
+
+	// ***************** Constructors ********************** //
+	/*
+	 * Construct a ray with a initial point and direction vector.
+	 * @param vec of direction .
+	 * 
+	 * @param p initial point 
+	 */
+	public Ray(Point3D p, Vector vec) {
+
+		this.dir = vec.normalized();
+		this.p = p;
+	}
+
+	// ***************** Getters ********************** //
+	/**
+	 * Getter of direction vector
+	 * @return direction vector
+	 */
+	public Vector getDir() {
+		return dir;
+	}
+	/**
+	 * Getter of initial point
+	 * @return initial point
+	 */
+	public Point3D getBasePoint() {
+		return p;
+	}
+
+	// ***************** Administration ******************** //
+	/**
+	 * equal function
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (p.equals(other)) {
+			if (dir.equals(other)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "from " + p + " to " + dir;
+	}
+}
+package primitives;
+/**
+ * Class represents 3D space ray.
+ * @author bassi
+ * @author asaf0
+ */
+public class Ray {
+	private Vector dir;
+	private Point3D p;
+
+	// ***************** Constructors ********************** //
+	/*
+	 * Construct a ray with a initial point and direction vector.
+	 * @param vec of direction .
+	 * 
+	 * @param p initial point 
+	 */
+	public Ray(Point3D p, Vector vec) {
+
+		this.dir = vec.normalized();
+		this.p = p;
+	}
+
+	// ***************** Getters ********************** //
+	/**
+	 * Getter of direction vector
+	 * @return direction vector
+	 */
+	public Vector getDir() {
+		return dir;
+	}
+	/**
+	 * Getter of initial point
+	 * @return initial point
+	 */
+	public Point3D getBasePoint() {
+		return p;
+	}
+
+	// ***************** Administration ******************** //
+	/**
+	 * equal function
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (p.equals(other)) {
+			if (dir.equals(other)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "from " + p + " to " + dir;
+	}
+}
+package primitives;
+/**
+ * Class represents 3D space ray.
+ * @author bassi
+ * @author asaf0
+ */
+public class Ray {
+	private Vector dir;
+	private Point3D p;
+
+	// ***************** Constructors ********************** //
+	/*
+	 * Construct a ray with a initial point and direction vector.
+	 * @param vec of direction .
+	 * 
+	 * @param p initial point 
+	 */
+	public Ray(Point3D p, Vector vec) {
+
+		this.dir = vec.normalized();
+		this.p = p;
+	}
+
+	// ***************** Getters ********************** //
+	/**
+	 * Getter of direction vector
+	 * @return direction vector
+	 */
+	public Vector getDir() {
+		return dir;
+	}
+	/**
+	 * Getter of initial point
+	 * @return initial point
+	 */
 	public Point3D getBasePoint() {
 		return p;
 	}
