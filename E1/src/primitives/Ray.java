7d8ef14f83f794ff9dc095a32b564033efe3ca2f
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-09 20:24:28 +0200

    Update Ray.java

--- E1/src/primitives/Ray.java
+++ E1/src/primitives/Ray.java
@@ -1,6 +1,8 @@
 package primitives;
+
 /**
  * Class represents 3D space ray.
+ * 
  * @author bassi
  * @author asaf0
  */
@@ -11,9 +13,10 @@ public class Ray {
 	// ***************** Constructors ********************** //
 	/*
 	 * Construct a ray with a initial point and direction vector.
+	 * 
 	 * @param vec of direction .
 	 * 
-	 * @param p initial point 
+	 * @param p initial point
 	 */
 	public Ray(Point3D p, Vector vec) {
 
@@ -24,129 +27,16 @@ public class Ray {
 	// ***************** Getters ********************** //
 	/**
 	 * Getter of direction vector
-	 * @return direction vector
-	 */
-	public Vector getDir() {
-		return dir;
-	}
-	/**
-	 * Getter of initial point
-	 * @return initial point
-	 */
-	public Point3D getBasePoint() {
-		return p;
-	}
-
-	// ***************** Administration ******************** //
-	/**
-	 * equal function
-	 */
-	@Override
-	public boolean equals(Object other) {
-		if (p.equals(other)) {
-			if (dir.equals(other)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public String toString() {
-		return "from " + p + " to " + dir;
-	}
-}
-package primitives;
-/**
- * Class represents 3D space ray.
- * @author bassi
- * @author asaf0
- */
-public class Ray {
-	private Vector dir;
-	private Point3D p;
-
-	// ***************** Constructors ********************** //
-	/*
-	 * Construct a ray with a initial point and direction vector.
-	 * @param vec of direction .
 	 * 
-	 * @param p initial point 
-	 */
-	public Ray(Point3D p, Vector vec) {
-
-		this.dir = vec.normalized();
-		this.p = p;
-	}
-
-	// ***************** Getters ********************** //
-	/**
-	 * Getter of direction vector
 	 * @return direction vector
 	 */
 	public Vector getDir() {
 		return dir;
 	}
+
 	/**
 	 * Getter of initial point
-	 * @return initial point
-	 */
-	public Point3D getBasePoint() {
-		return p;
-	}
-
-	// ***************** Administration ******************** //
-	/**
-	 * equal function
-	 */
-	@Override
-	public boolean equals(Object other) {
-		if (p.equals(other)) {
-			if (dir.equals(other)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public String toString() {
-		return "from " + p + " to " + dir;
-	}
-}
-package primitives;
-/**
- * Class represents 3D space ray.
- * @author bassi
- * @author asaf0
- */
-public class Ray {
-	private Vector dir;
-	private Point3D p;
-
-	// ***************** Constructors ********************** //
-	/*
-	 * Construct a ray with a initial point and direction vector.
-	 * @param vec of direction .
 	 * 
-	 * @param p initial point 
-	 */
-	public Ray(Point3D p, Vector vec) {
-
-		this.dir = vec.normalized();
-		this.p = p;
-	}
-
-	// ***************** Getters ********************** //
-	/**
-	 * Getter of direction vector
-	 * @return direction vector
-	 */
-	public Vector getDir() {
-		return dir;
-	}
-	/**
-	 * Getter of initial point
 	 * @return initial point
 	 */
 	public Point3D getBasePoint() {
@@ -154,67 +44,6 @@ public class Ray {
 	}
 
 	// ***************** Administration ******************** //
-	/**
-	 * equal function
-	 */
-	@Override
-	public boolean equals(Object other) {
-		if (p.equals(other)) {
-			if (dir.equals(other)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public String toString() {
-		return "from " + p + " to " + dir;
-	}
-}
-package primitives;
-/**
- * Class represents 3D space ray.
- * @author bassi
- * @author asaf0
- */
-public class Ray {
-	private Vector dir;
-	private Point3D p;
-
-	// ***************** Constructors ********************** //
-	/*
-	 * Construct a ray with a initial point and direction vector.
-	 * @param vec of direction .
-	 * 
-	 * @param p initial point 
-	 */
-	public Ray(Point3D p, Vector vec) {
-
-		this.dir = vec.normalized();
-		this.p = p;
-	}
-
-	// ***************** Getters ********************** //
-	/**
-	 * Getter of direction vector
-	 * @return direction vector
-	 */
-	public Vector getDir() {
-		return dir;
-	}
-	/**
-	 * Getter of initial point
-	 * @return initial point
-	 */
-	public Point3D getBasePoint() {
-		return p;
-	}
-
-	// ***************** Administration ******************** //
-	/**
-	 * equal function
-	 */
 	@Override
 	public boolean equals(Object other) {
 		if (p.equals(other)) {
