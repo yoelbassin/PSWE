db559f52c3bcb6f8c258765c11ca88bc202382e6
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-09 16:31:01 +0200

    Update Triangle.java

--- E1/src/geometries/Triangle.java
+++ E1/src/geometries/Triangle.java
@@ -5,7 +5,14 @@ import java.util.List;
 import primitives.*;
 
 public class Triangle extends Polygon {
-
+	// ***************** Constructor ********************** //
+	/**
+	 * constructs a triangle from three points
+	 * 
+	 * @param p1, first point
+	 * @param p2, second point
+	 * @param p3, third point
+	 */
 	public Triangle(Point3D p1, Point3D p2, Point3D p3) {
 		super(p1, p2, p3);
 	}
