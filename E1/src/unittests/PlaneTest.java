837ef4bb9d395a0f5287920779cbd613e7cb355d
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-24 14:23:00 +0200

    Update PlaneTest.java

--- E1/src/unittests/PlaneTest.java
+++ E1/src/unittests/PlaneTest.java
@@ -1,31 +1,55 @@
 package unittests;
 
 import geometries.*;
+
 import static org.junit.Assert.*;
+
 import primitives.*;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PlaneTest {
-	/**
-	 * plane with same point in constructor
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void testPlaneZero() {
-		Point3D p1 = new Point3D(0, 0, 0);
-		Point3D p2 = new Point3D(0, 0, 0);
-		Point3D p3 = new Point3D(0, 0, 0);
-		Plane plane = new Plane(p1, p2, p3);
-	}
 
-	/**
-	 * test of get normal function 
-	 */
-	@Test
-	public void testGetNormal() {
-		Point3D p = new Point3D(0, 0, 0);
-		Vector vec = new Vector(0, 1, 0);
-		Plane plane = new Plane(p, vec);
-		assertEquals("Get normal function error",vec, plane.getNormal());
-	}
+    /**
+     * test Method for {@link geomtries.Plane#getNormal(geomtries.Plane)}
+     */
+    @Test
+    public void testGetNormal() {
+        Point3D p = new Point3D(0, 0, 0);
+        Vector vec = new Vector(0, 1, 0);
+        Plane plane = new Plane(p, vec);
+        assertEquals("Get normal function error", vec, plane.getNormal());
+    }
 
+    static Point3D p = new Point3D(1, 1, 0);
+    static Vector dir = new Vector(0, 0, 1);
+
+    @Test
+    public void testFindIntersections() {
+        Plane polygon = new Plane(p, dir);
+        Ray ray = new Ray(new Point3D(0, 0, 1), new Vector(1, 1, -1));
+        Point3D intersection = new Point3D(1, 1, 0);
+        List<Point3D> intersections = Arrays.asList(intersection);
+        assertEquals("Find intersections function error", intersections, polygon.findIntersections(ray)); //EP
+        ray = new Ray(new Point3D(0, 0, 1), new Vector(1, 1, 1));
+        assertEquals("Find intersections function error", null, polygon.findIntersections(ray)); //EP
+        ray = new Ray(new Point3D(0, 0, 0), new Vector(1, 1, 0));
+        assertEquals("Find intersections function error", null, polygon.findIntersections(ray)); //BVA ray is parallel and included in the plane
+        ray = new Ray(new Point3D(0, 0, 1), new Vector(1, 1, 0));
+        assertEquals("Find intersections function error", null, polygon.findIntersections(ray)); //BVA ray is parallel and not included in the plane
+        ray = new Ray(new Point3D(0, 0, 1), new Vector(0, 0, 1));
+        assertEquals("Find intersections function error", null, polygon.findIntersections(ray)); //BVA ray is orthogonal and after p0
+        ray = new Ray(new Point3D(0, 0, -1), new Vector(0, 0, 1));
+        intersections = Arrays.asList(new Point3D(0, 0, 0));
+        assertEquals("Find intersections function error", intersections, polygon.findIntersections(ray)); //BVA ray is orthogonal and before p0
+        ray = new Ray(new Point3D(0, 0, 0), new Vector(0, 0, 1));
+        assertEquals("Find intersections function error", intersections, polygon.findIntersections(ray)); //BVA ray is orthogonal and in p0
+        ray = new Ray(new Point3D(0, 0, 0), new Vector(1, 1, 1));
+        assertEquals("Find intersections function error", intersections, polygon.findIntersections(ray)); //BVA ray begins in the plane
+        ray = new Ray(new Point3D(1, 1, 0), new Vector(1, 1, 1));
+        intersections = Arrays.asList(new Point3D(1, 1, 0));
+        assertEquals("Find intersections function error", intersections, polygon.findIntersections(ray)); //BVA ray begins in the reference point of the plane
+    }
 }
