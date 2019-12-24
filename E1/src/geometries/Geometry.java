e542258c1bf93d2af5afff617f3a25234d2d4954
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-24 14:26:24 +0200

    Update Geometry.java

--- E1/src/geometries/Geometry.java
+++ E1/src/geometries/Geometry.java
@@ -2,10 +2,11 @@ package geometries;
 
 import primitives.*;
 
-public interface Geometry {
+public interface Geometry extends Intersectable {
 	/**
-	 * Calculates unit vector orthogonal to the tangent plane at the point (which
-	 * is called - normal)
+	 * Calculates unit vector orthogonal to the tangent plane at the point (which is
+	 * called - normal)
+	 * 
 	 * @param p point of tangent...
 	 * @return normal vector
 	 */
