3ddc40fd5720f0ed67ed1dfe8a1317d33f4e6137
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-24 14:29:26 +0200

    Update TubeTest.java

--- E1/src/unittests/TubeTest.java
+++ E1/src/unittests/TubeTest.java
@@ -3,7 +3,7 @@ package unittests;
 import static org.junit.Assert.*;
 import primitives.*;
 import geometries.*;
-import org.junit.Test; 
+import org.junit.Test;
 
 public class TubeTest {
 	public static Point3D basePoint = new Point3D(0, 0, 0);
@@ -11,25 +11,20 @@ public class TubeTest {
 	public static Ray asix = new Ray(basePoint, vec);
 
 	/**
-	 * Test of creating a tube with radius smaller than zero
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void testTube() {
-		Tube tube = new Tube(asix, 0);
-	}
-
-	/**
-	 * Tube normal function test
+	 * test Method for {@link geomtries.Tube#getNormal(geomtries.Tube)}
 	 */
 	@Test
 	public void testGetNormal() {
+		// Test of normal on the tube
 		Tube tube = new Tube(asix, 1);
 		Point3D p = new Point3D(1, 2, 0);
 		Vector actual = new Vector(1, 0, 0);
-		assertEquals("Normal function error", tube.getNormal(p), actual);// Test of normal on the tube
-		p = new Point3D(0, 0, 0);
-		actual = new Vector(0, 1, 0);
-		assertEquals("Normal function error", tube.getNormal(p), actual);// Test of normal on the base of the tube
+		assertEquals("Normal function error", tube.getNormal(p), actual);
+	}
+
+	@Test
+	public void testfindIntersections() {
+		fail();
 	}
 
 }
