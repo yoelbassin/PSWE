9cb8471e6d7c4b628879b18e62f4849fb822879f
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-29 13:58:40 +0200

    Update Intersectable.java

--- E1/src/geometries/Intersectable.java
+++ E1/src/geometries/Intersectable.java
@@ -5,6 +5,12 @@ import java.util.List;
 import primitives.*;
 
 public interface Intersectable {
+	/**
+	 * finds intersections of the ray with the Geometry
+	 *
+	 * @param ray
+	 * @return intersection point
+	 */
 	List<Point3D> findIntersections(Ray ray);
 
 }
