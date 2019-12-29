d5fb91d3d116f0add8c955b4a8eb700ebc238885
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-29 14:02:41 +0200

    Update PolygonTest.java

--- E1/src/unittests/PolygonTest.java
+++ E1/src/unittests/PolygonTest.java
@@ -1,22 +1,45 @@
 package unittests;
 
 import static org.junit.Assert.*;
+
 import primitives.*;
 import geometries.*;
 import org.junit.Test;
 
-public class PolygonTest {
+import java.util.Arrays;
+import java.util.List;
 
-	/**
-	 * Polygon normal test
-	 */
-	@Test
-	public void testGetNormal() {
-		Point3D p1 = new Point3D(0, 0, 0);
-		Point3D p2 = new Point3D(0, 3, 0);
-		Point3D p3 = new Point3D(4, 0, 0);
-		Polygon polygon = new Polygon(p1, p2, p3);
-		Vector normal = new Vector(0, 0, 1);
-		assertEquals("Get normal function error", normal, polygon.getNormal(new Point3D(1, 1, 0)));
-	}
+public class PolygonTest {
+    static Point3D p1 = new Point3D(0, 0, 0);
+    static Point3D p2 = new Point3D(0, 1, 0);
+    static Point3D p3 = new Point3D(1, 0, 0);
+
+    /**
+     * test Method for {@link geometries.Polygon (geomtries.Polygon)}
+     */
+    @Test
+    public void testConstructor() {
+        try {
+            // Test of the polygon constructor
+            Point3D p4 = new Point3D(1, 1, 0);
+            Polygon polygon = new Polygon(p1, p2, p3, p4);
+            assertTrue(true);
+        } catch (IllegalArgumentException e) {
+            fail("threw an unwanted exception");
+        }
+        try {
+            Point3D p4 = new Point3D(1, 1, 3);
+            Polygon polygon = new Polygon(p1, p2, p3, p4);
+            fail("didn't throw exception"); //BVA constructing a polygon with a point not on the same plane as the others
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+        try {
+            Point3D p4 = new Point3D(1, 1, -3);
+            Polygon polygon = new Polygon(p1, p2, p3, p4);
+            fail("didn't throw exception"); //BVA constructing a polygon with a point not on the same plane as the others
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
 }
