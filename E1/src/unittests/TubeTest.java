404ae219a10d34d9fc5e6c93c02179476bf670cb
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2019-12-29 14:03:10 +0200

    Update TubeTest.java

--- E1/src/unittests/TubeTest.java
+++ E1/src/unittests/TubeTest.java
@@ -1,30 +1,25 @@
 package unittests;
 
 import static org.junit.Assert.*;
+
 import primitives.*;
 import geometries.*;
 import org.junit.Test;
 
 public class TubeTest {
-	public static Point3D basePoint = new Point3D(0, 0, 0);
-	public static Vector vec = new Vector(0, 1, 0);
-	public static Ray asix = new Ray(basePoint, vec);
+    public static Point3D basePoint = new Point3D(0, 0, 0);
+    public static Vector vec = new Vector(0, 1, 0);
+    public static Ray asix = new Ray(basePoint, vec);
 
-	/**
-	 * test Method for {@link geomtries.Tube#getNormal(geomtries.Tube)}
-	 */
-	@Test
-	public void testGetNormal() {
-		// Test of normal on the tube
-		Tube tube = new Tube(asix, 1);
-		Point3D p = new Point3D(1, 2, 0);
-		Vector actual = new Vector(1, 0, 0);
-		assertEquals("Normal function error", tube.getNormal(p), actual);
-	}
-
-	@Test
-	public void testfindIntersections() {
-		fail();
-	}
-
+    /**
+     * test Method for {@link geometries.Tube#getNormal (geomtries.Tube)}
+     */
+    @Test
+    public void testGetNormal() {
+        // Test of normal on the tube
+        Tube tube = new Tube(asix, 1);
+        Point3D p = new Point3D(1, 2, 0);
+        Vector actual = new Vector(1, 0, 0);
+        assertEquals("Normal function error", tube.getNormal(p), actual);
+    }
 }
