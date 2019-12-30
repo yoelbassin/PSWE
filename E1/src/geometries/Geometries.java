90f8d3a5d8bf8b3642f2d7ce870e9d20076cfc75
asafJct <44846049+asafJct@users.noreply.github.com>
2019-12-30 15:12:26 +0200

    Update Geometries.java

--- E1/src/geometries/Geometries.java
+++ E1/src/geometries/Geometries.java
@@ -7,15 +7,20 @@ import unittests.GeometriesTest;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * class represents a list 3D space shapes which could intersect a ray.  
+ * @author bassi
+ * @author asaf0
+ *
+ */
 public class Geometries implements Intersectable {
     List<Intersectable> shapes = new ArrayList<Intersectable>();
 
     /**
-     * default constructor
+     * Default constructor
      * constructs the list as an empty ArrayList
      * ArrayList - get function is O(1) and add function is O(n)
-     * we chose it because get function used more
+     * we have chosen to use it because get function used more
      */
     public Geometries() {
         shapes = new ArrayList<Intersectable>();
@@ -24,7 +29,7 @@ public class Geometries implements Intersectable {
     /**
      * constructs a list with geometries (one or more)
      *
-     * @param geometries
+     * @param list of geometry shapes.
      */
     public Geometries(Intersectable... geometries) {
         for (int i = 0; i < geometries.length; ++i) {
@@ -47,8 +52,8 @@ public class Geometries implements Intersectable {
     /**
      * finds intersections of the ray with the geometries that are in the list
      *
-     * @param ray
-     * @return intersection point
+     * @param ray in space.
+     * @return intersection points
      */
     public List<Point3D> findIntersections(Ray ray) {
         List<Point3D> intersections = new ArrayList<Point3D>();
