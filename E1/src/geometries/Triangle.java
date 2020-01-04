b670970a72a07a70c9db7607b5fd8fdae56e568f
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-04 18:58:31 +0200

    Update Triangle.java

--- E1/src/geometries/Triangle.java
+++ E1/src/geometries/Triangle.java
@@ -18,58 +18,34 @@ public class Triangle extends Polygon {
         super(p1, p2, p3);
     }
     // ***************** Operations ******************** //
-
     /**
-     * function finds intersection's of the ray with the triangle
+     * finds intersection's of the ray with the triangle
      *
      * @param ray
      * @return intersection point
      */
     @Override
-	public List<Point3D> findIntersections(Ray ray) {
-		if (this._plane.findIntersections(ray) ==null) // if there are no intersections with the plane or			
-							       //  the ray basePoint is on the triangle
-			return null;// then return null
-		Point3D p0 = ray.getBasePoint();
-		Vector v1 = this._points.get(0).subtract(p0); // vector v1 base point at p0 and head point in p1
-		Vector v2 = this._points.get(1).subtract(p0);// vector v2 base point at p0 and head point in p2
-		Vector v3 = this._points.get(2).subtract(p0);// vector v3 base point at p0 and head point in p3
-		Vector N1 = v1.crossProduct(v2).normalize(); // N1=V1xV2
-		Vector N2 = v2.crossProduct(v3).normalize(); // N2=V2xV3
-		Vector N3 = v3.crossProduct(v1).normalize(); // N3=V3xV1
-		List<Point3D> intersections = this._plane.findIntersections(ray);// check if the intersection point is on the
-																			// triangle
-		Vector p = intersections.get(0).subtract(p0); // p vector from p0 to p1
-		if (p.dotProduct(N1) > 0 && p.dotProduct(N2) > 0 && p.dotProduct(N3) > 0) // case that p*N1, p*N2, p*N3 > 0
-			return intersections;
-		if (p.dotProduct(N1) < 0 && p.dotProduct(N2) < 0 && p.dotProduct(N3) < 0) // case that p*N1 , p*N2 , p*N3 < 0
-			return intersections;
-		return null;//Other cases
-	}
-    /*
-    @Override
     public List<Point3D> findIntersections(Ray ray) {
-        if (this._plane.findIntersections(ray) == null) //if there are no intersections with the plane, or the ray's base is on the triangle return null
-            return null;
-        //check if the intersection point is on the triangle
         List<Point3D> intersections = this._plane.findIntersections(ray);
-        Point3D intersectionPoint = intersections.get(0);
+        if (intersections == null) // if there are no intersections with the plane, or the ray's
+            // base is on the triangle
+            return null;// then return null
+        // check if the intersection point is on the triangle
         Point3D p0 = ray.getBasePoint();
         Point3D p1 = this._points.get(0);
         Point3D p2 = this._points.get(1);
         Point3D p3 = this._points.get(2);
-        Vector v1 = p1.subtract(p0); //v1=p1-p0
-        Vector v2 = p2.subtract(p0); //v2=p2-p0
-        Vector v3 = p3.subtract(p0); //v3=p3-p0
-        Vector N1 = v1.crossProduct(v2).normalize(); //N1=V1xV2
-        Vector N2 = v2.crossProduct(v3).normalize(); //N2=V2xV3
-        Vector N3 = v3.crossProduct(v1).normalize(); //N3=V3xV1
-        Vector p = intersectionPoint.subtract(p0); //p-p0
-        if (p.dotProduct(N1) > 0 && p.dotProduct(N2) > 0 && p.dotProduct(N3) > 0) //p*N1, p*N2, p*N3 > 0
-            return intersections;
-        if (p.dotProduct(N1) < 0 && p.dotProduct(N2) < 0 && p.dotProduct(N3) < 0) //p*N1, p*N2, p*N3 < 0
+        Vector v1 = p1.subtract(p0); // v1=p1-p0
+        Vector v2 = p2.subtract(p0); // v2=p2-p0
+        Vector v3 = p3.subtract(p0); // v3=p3-p0
+        Vector N1 = v1.crossProduct(v2).normalize(); // N1=V1xV2
+        Vector N2 = v2.crossProduct(v3).normalize(); // N2=V2xV3
+        Vector N3 = v3.crossProduct(v1).normalize(); // N3=V3xV1
+        Vector p = intersections.get(0).subtract(p0); // p-p0
+        if ((p.dotProduct(N1) > 0 && p.dotProduct(N2) > 0 && p.dotProduct(N3) > 0)
+                || (p.dotProduct(N1) < 0 && p.dotProduct(N2) < 0 && p.dotProduct(N3) < 0)) // p*N1, p*N2, p*N3 > 0 or
+            // p*N1, p*N2, p*N3 < 0
             return intersections;
         return null;
     }
-    */
 }
