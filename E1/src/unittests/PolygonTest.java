7e879e2294a896f621336edbd073bb4a0791593f
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-06 18:46:10 +0200

    Update PolygonTest.java

--- E1/src/unittests/PolygonTest.java
+++ E1/src/unittests/PolygonTest.java
@@ -10,19 +10,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PolygonTest {
-    static Point3D p1 = new Point3D(0, 0, 0);
-    static Point3D p2 = new Point3D(0, 1, 0);
-    static Point3D p3 = new Point3D(1, 0, 0);
-
     /**
      * test Method for {@link geometries.Polygon (geomtries.Polygon)}
      */
     @Test
     public void testConstructor() {
+        Point3D p1 = new Point3D(0, 0, 0);
+        Point3D p2 = new Point3D(1, 0, 0);
+        Point3D p3 = new Point3D(1, 1, 0);
         // ****** EP ******//
         try {
             // Test of the polygon constructor
-            Point3D p4 = new Point3D(1, 1, 0);
+            Point3D p4 = new Point3D(0, 1, 0);
             Polygon polygon = new Polygon(p1, p2, p3, p4);
             assertTrue(true);
         } catch (IllegalArgumentException e) {
@@ -30,7 +29,7 @@ public class PolygonTest {
         }
         // ****** BVA ******//
         try {
-            Point3D p4 = new Point3D(1, 1, 3);
+            Point3D p4 = new Point3D(0, 1, 3);
             Polygon polygon = new Polygon(p1, p2, p3, p4);
             fail("didn't throw exception"); // BVA constructing a polygon with a point not on the same plane as the
             // others
@@ -38,7 +37,7 @@ public class PolygonTest {
             assertTrue(true);
         }
         try {
-            Point3D p4 = new Point3D(1, 1, -3);
+            Point3D p4 = new Point3D(0, 1, -3);
             Polygon polygon = new Polygon(p1, p2, p3, p4);
             fail("didn't throw exception"); // BVA constructing a polygon with a point not on the same plane as the
             // others
@@ -48,21 +47,30 @@ public class PolygonTest {
     }
 
     /**
-     * test Method for {@link geometries.Polygon (geomtries.Polygon)}
+     * test Method for {@link geometries.Polygon#findIntersections}  (geomtries.Polygon)}
      */
     @Test
     public void testFindIntersections() {
-        Point3D p4 = new Point3D(1, 1, 0);
-        Polygon polygon = new Polygon(p1, p2, p3, p4);
+        Point3D p1 = new Point3D(0, 0, 0);
+        Point3D p2 = new Point3D(1, 0, 0);
+        Point3D p3 = new Point3D(1, 1, 0);
+        Point3D p4 = new Point3D(0, 1, 0);
+        Polygon square = new Polygon(p1, p2, p3, p4);
         Ray ray = new Ray(new Point3D(0.25, 0.25, -1), new Vector(0, 0, 1));
         List<Point3D> intersections = Arrays.asList(new Point3D(0.25, 0.25, 0));
-        polygon.findIntersections(ray).get(0);
-        assertEquals("Polygon findIntersection error ", intersections, polygon.findIntersections(ray));
-        ray = new Ray(new Point3D(0.5, 0.5, -1), new Vector(0, 0, 1));
-        intersections = Arrays.asList(new Point3D(0.5, 0.5, 0));
-        assertEquals("Polygon findIntersection error ", intersections, polygon.findIntersections(ray));
+        assertEquals("Polygon findIntersection error ", intersections, square.findIntersections(ray)); //inside square
         ray = new Ray(new Point3D(-1, -1, -1), new Vector(0, 0, 1));
-        assertEquals("Polygon findIntersection error ", null, polygon.findIntersections(ray));
-    }
-
+        assertEquals("Polygon findIntersection error ", null, square.findIntersections(ray)); //outside square
+        p1 = new Point3D(-2, 0, 0);
+        p2 = new Point3D(-1, 2, 0);
+        p3 = new Point3D(1, 2, 0);
+        p4 = new Point3D(2, 0, 0);
+        Point3D p5 = new Point3D(0, -2, 0);
+        Polygon pentagon = new Polygon(p1,p2,p3,p4,p5);
+        ray = new Ray(new Point3D(0, 0, -1), new Vector(0, 0, 1));
+        intersections = Arrays.asList(new Point3D(0, 0, 0));
+        assertEquals("Polygon findIntersection error ", intersections, pentagon.findIntersections(ray)); //inside pentagon
+        ray = new Ray(new Point3D(-3, 3, -1), new Vector(0, 0, 1));
+        assertEquals("Polygon findIntersection error ", null, pentagon.findIntersections(ray)); //outside pentagon
+        }
 }
