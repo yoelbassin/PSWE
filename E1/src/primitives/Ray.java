cd22b466782342a214712b15aeaa4363f3797af0
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-06 14:23:48 +0200

    Update Ray.java

--- E1/src/primitives/Ray.java
+++ E1/src/primitives/Ray.java
@@ -2,60 +2,55 @@ package primitives;
 
 /**
  * Class represents 3D space ray.
- * 
- * @author bassi
- * @author asaf0
  */
 public class Ray {
-	private Vector dir;
-	private Point3D p;
+    private Vector dir;
+    private Point3D p;
 
-	// ***************** Constructors ********************** //
-	/*
-	 * Construct a ray with a initial point and direction vector.
-	 * 
-	 * @param vec of direction .
-	 * 
-	 * @param p initial point
-	 */
-	public Ray(Point3D p, Vector vec) {
+    // ***************** Constructors ********************** //
+    /*
+     * Construct a ray with a initial point and direction vector.
+     *
+     * @param vec of direction .
+     *
+     * @param p initial point
+     */
+    public Ray(Point3D p, Vector vec) {
+        this.dir = vec.normalized();
+        this.p = p;
+    }
+    // ***************** Getters ********************** //
 
-		this.dir = vec.normalized();
-		this.p = p;
-	}
+    /**
+     * Getter of direction vector
+     *
+     * @return direction vector
+     */
+    public Vector getDir() {
+        return dir;
+    }
 
-	// ***************** Getters ********************** //
-	/**
-	 * Getter of direction vector
-	 * 
-	 * @return direction vector
-	 */
-	public Vector getDir() {
-		return dir;
-	}
+    /**
+     * Getter of initial point
+     *
+     * @return initial point
+     */
+    public Point3D getBasePoint() {
+        return p;
+    }
 
-	/**
-	 * Getter of initial point
-	 * 
-	 * @return initial point
-	 */
-	public Point3D getBasePoint() {
-		return p;
-	}
+    // ***************** Administration ******************** //
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null) return false;
+        if (!(other instanceof Ray)) return false;
+        Ray obj = (Ray) other;
+        return dir.equals(obj.getDir()) && p.equals(obj.p);
+    }
 
-	// ***************** Administration ******************** //
-	@Override
-	public boolean equals(Object other) {
-		if (p.equals(other)) {
-			if (dir.equals(other)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public String toString() {
-		return "from " + p + " to " + dir;
-	}
+    @Override
+    public String toString() {
+        return "[" + p + dir + ")";
+    }
 }
