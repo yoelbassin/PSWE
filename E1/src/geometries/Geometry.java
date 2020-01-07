6be3e3abafdb77a0ddb0736b591ee9dc711f0104
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-07 18:26:47 +0200

    Update Geometry.java

--- E1/src/geometries/Geometry.java
+++ E1/src/geometries/Geometry.java
@@ -3,7 +3,9 @@ package geometries;
 import primitives.*;
 
 /**
- * Interface of the whole geometry shapes which gives the the ability to find a normal and intersec with rays.
+ * Interface of the whole geometry shapes which gives the the ability to find a
+ * normal and intersect with rays.
+ *
  * @author bassi
  * @author asaf0
  *
@@ -12,7 +14,7 @@ public interface Geometry extends Intersectable {
 	/**
 	 * Calculates unit vector orthogonal to the tangent plane at the point (which is
 	 * called - normal)
-	 * 
+	 *
 	 * @param p point of tangent...
 	 * @return normal vector
 	 */
