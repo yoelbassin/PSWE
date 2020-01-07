9f5031152758119d97a162500d550a0dec95bede
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-07 18:30:56 +0200

    Update CameraTest.java

--- E1/src/unittests/CameraTest.java
+++ E1/src/unittests/CameraTest.java
@@ -22,7 +22,7 @@ public class CameraTest {
     @Test
     public void cameraTest() {
         try {
-            Camera camera = new Camera(p0, vUp, vTo);
+            new Camera(p0, vUp, vTo);
             assertTrue(true);
         } catch (Exception e) {
             fail("Unwanted exception");
@@ -30,7 +30,7 @@ public class CameraTest {
         try {
             Vector vUpWrong = new Vector(0, -1, 0);
             Vector vToWrong = new Vector(0, 1, -1);
-            Camera camera = new Camera(p0, vUpWrong, vToWrong);
+            new Camera(p0, vUpWrong, vToWrong);
             fail("Must throw an exception");
         } catch (Exception e) {
             assertTrue(true);
@@ -38,24 +38,27 @@ public class CameraTest {
     }
 
     /**
-     * test Method for {@link elements.Camera#constructRayThroughPixel (elements.Camera)}
+     * test Method for
+     * {@link elements.Camera#constructRayThroughPixel (elements.Camera)}
      */
     @Test
     public void constructRayThroughPixelTest() {
         Camera camera = new Camera(p0, vUp, vTo);
+
         // ********** 3x3 view plane **********//
         Ray ray = camera.constructRayThroughPixel(3, 3, 0, 0, 1, 3, 3);
-        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1, -1, -1)), ray); //Corner pixel case
+        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1, -1, -1)), ray); // Corner pixel case
         ray = camera.constructRayThroughPixel(3, 3, 0, 1, 1, 3, 3);
-        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1, 0, -1)), ray); //Side pixel case
+        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1, 0, -1)), ray); // Side pixel case
         ray = camera.constructRayThroughPixel(3, 3, 1, 1, 1, 3, 3);
-        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(0, 0, -1)), ray); //Inside pixel case
+        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(0, 0, -1)), ray); // Inside pixel case
+
         // ********** 4x4 view plane **********//
         ray = camera.constructRayThroughPixel(4, 4, 0, 0, 1, 4, 4);
-        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1.5, -1.5, -1)), ray); //Corner pixel case
+        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1.5, -1.5, -1)), ray); // Corner pixel case
         ray = camera.constructRayThroughPixel(4, 4, 0, 1, 1, 4, 4);
-        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1.5, -0.5, -1)), ray); //Inside pixel case
+        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(1.5, -0.5, -1)), ray); // Inside pixel case
         ray = camera.constructRayThroughPixel(4, 4, 1, 1, 1, 4, 4);
-        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(0.5, -0.5, -1)), ray); //Side pixel case
+        assertEquals(new Ray(new Point3D(0, 0, 0), new Vector(0.5, -0.5, -1)), ray); // Side pixel case
     }
 }
