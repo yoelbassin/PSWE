0446d6bacfdb760e005c5e06acc961b0947e9946
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-07 18:26:34 +0200

    Update Geometries.java

--- E1/src/geometries/Geometries.java
+++ E1/src/geometries/Geometries.java
@@ -2,13 +2,13 @@ package geometries;
 
 import primitives.Point3D;
 import primitives.Ray;
-import unittests.GeometriesTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * class represents a list 3D space shapes which could intersect a ray.
+ *
  * @author bassi
  * @author asaf0
  *
@@ -17,10 +17,9 @@ public class Geometries implements Intersectable {
     List<Intersectable> shapes = new ArrayList<Intersectable>();
 
     /**
-     * Default constructor
-     * constructs the list as an empty ArrayList
-     * ArrayList - get function is O(1) and add function is O(n)
-     * we have chosen to use it because get function used more
+     * Default constructor constructs the list as an empty ArrayList ArrayList - get
+     * function is O(1) and add function is O(n) we have chosen to use it because
+     * get function used more
      */
     public Geometries() {
         shapes = new ArrayList<Intersectable>();
@@ -58,10 +57,12 @@ public class Geometries implements Intersectable {
     public List<Point3D> findIntersections(Ray ray) {
         List<Point3D> intersections = new ArrayList<Point3D>();
         for (int i = 0; i < shapes.size(); ++i) {
-            if (shapes.get(i).findIntersections(ray) != null) //if an intersection is found, add it to the list
+            // if an intersection is found, add it to the list
+            if (shapes.get(i).findIntersections(ray) != null)
                 intersections.add(shapes.get(i).findIntersections(ray).get(0));
         }
-        if (intersections.size() == 0) //if no intersections were found, return null
+        // if no intersections were found, return null
+        if (intersections.size() == 0)
             return null;
         return intersections;
     }
