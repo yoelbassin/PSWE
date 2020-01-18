5f501ab405e8a416cd35e4207085c3f1bdd10c68
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-18 18:26:34 +0200

    Update AmbientLight.java

--- E1/src/elements/AmbientLight.java
+++ E1/src/elements/AmbientLight.java
@@ -2,12 +2,28 @@ package elements;
 
 import primitives.Color;
 
-public class AmbientLight {
-    Color intensity;
-    public AmbientLight(Color i, double k){
+/**
+ * class represents an ambient light in a scene
+ */
+public class AmbientLight extends Light {
+    // ***************** Constructors ********************** //
+
+    /**
+     * sets the ambient light of the picture by color and Attenuation coefficient
+     *
+     * @param i - Light color
+     * @param k - Attenuation coefficient
+     */
+    public AmbientLight(Color i, double k) {
         intensity = i.scale(k);
     }
+    // ***************** Getters/Setters ********************** //
 
+    /**
+     * getter for the ambient lighting
+     *
+     * @return light intensity
+     */
     public Color getIntensity() {
         return intensity;
     }
