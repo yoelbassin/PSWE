76a1c84d0607b5a4d567418285e55f2d4d7a6d27
asafJct <44846049+asafJct@users.noreply.github.com>
2020-01-21 20:09:05 +0200

    Update Material.java

--- E1/src/primitives/Material.java
+++ E1/src/primitives/Material.java
@@ -7,7 +7,8 @@ public class Material {
     double _kD;
     double _kS;
     int _nShininess;
-
+    double _kR;
+    double _kT;
     // ***************** Constructors ********************** //
     /**
      * creates a material for a geometry
@@ -20,6 +21,21 @@ public class Material {
         _kD = kd;
         _kS = ks;
         _nShininess = shininess;
+    }
+    /**
+     * creates a material for a geometry
+     *
+     * @param kd        - diffusion attenuation coefficient
+     * @param ks        - specular attenuation coefficient
+     * @param shininess - shininess power
+     * @param
+     */
+    public Material(double kd, double ks, int shininess,double kR,double kT) {
+        _kD = kd;
+        _kS = ks;
+        _nShininess = shininess;
+        _kR=kR;
+        _kT=kT;
     }
     // ***************** Getters/Setters ********************** //
 
@@ -40,6 +56,21 @@ public class Material {
     public double getKs() {
         return _kS;
     }
+    
+    /**
+     * Getter of the material's reflection coefficient 
+     * @return reflection coefficient 
+     */
+    public double getKr() {
+        return _kR;
+    }
+    /**
+     * Getter of material's reffraction coefficient
+     * @return reffraction coefficient
+     */
+    public double getKt() {
+        return _kT;
+    }
 
     /**
      * getter of shininess power
@@ -49,4 +80,5 @@ public class Material {
     public int getShininess() {
         return _nShininess;
     }
+    
 }
