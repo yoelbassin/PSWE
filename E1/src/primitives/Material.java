d19aad93372588d5bb178da94faaffb026c1003e
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-01-28 23:55:42 +0200

    Update Material.java

--- E1/src/primitives/Material.java
+++ E1/src/primitives/Material.java
@@ -4,88 +4,87 @@ package primitives;
  * class represents a material of a geometry
  */
 public class Material {
-	double _kD;
-	double _kS;
-	int _nShininess;
-	double _kR;
-	double _kT;
+    double _kD;
+    double _kS;
+    int _nShininess;
+    double _kR;
+    double _kT;
 
-	// ***************** Constructors ********************** //
-	/**
-	 * creates a material for a geometry and give default values to kR and kT
-	 *
-	 * @param kd        - diffusion attenuation coefficient
-	 * @param ks        - specular attenuation coefficient
-	 * @param shininess - shininess power
-	 */
-	public Material(double kd, double ks, int shininess) {
-		_kD = kd;
-		_kS = ks;
-		_nShininess = shininess;
-		_kR = 0;
-		_kT = 0;
-	}
+    // ***************** Constructors ********************** //
 
-	/**
-	 * creates a material for a geometry
-	 *
-	 * @param kd        - diffusion attenuation coefficient
-	 * @param ks        - specular attenuation coefficient
-	 * @param shininess - shininess power
-	 * @param
-	 */
-	public Material(double kd, double ks, int shininess, double kR, double kT) {
-		_kD = kd;
-		_kS = ks;
-		_nShininess = shininess;
-		_kR = kR;
-		_kT = kT;
-	}
-	// ***************** Getters/Setters ********************** //
+    /**
+     * creates a material for a geometry
+     *
+     * @param kd        - diffusion attenuation coefficient
+     * @param ks        - specular attenuation coefficient
+     * @param shininess - shininess power
+     */
+    public Material(double kd, double ks, int shininess) {
+        _kD = kd;
+        _kS = ks;
+        _nShininess = shininess;
+        _kR = 0;
+        _kT = 0;
+    }
 
-	/**
-	 * getter of diffusion attenuation coefficient
-	 *
-	 * @return diffusion attenuation coefficient
-	 */
-	public double getKd() {
-		return _kD;
-	}
+    /**
+     * creates a material for a geometry
+     *
+     * @param kd        - diffusion attenuation coefficient
+     * @param ks        - specular attenuation coefficient
+     * @param shininess - shininess power
+     * @param kr        - reflection coefficient
+     * @param kt        - transparency coefficient
+     */
+    public Material(double kd, double ks, int shininess, double kr, double kt) {
+        this(kd, ks, shininess);
+        _kR = kr;
+        _kT = kt;
+    }
+    // ***************** Getters/Setters ********************** //
 
-	/**
-	 * getter of specular attenuation coefficient
-	 *
-	 * @return specular attenuation coefficient
-	 */
-	public double getKs() {
-		return _kS;
-	}
+    /**
+     * getter of diffusion attenuation coefficient
+     *
+     * @return diffusion attenuation coefficient
+     */
+    public double getKd() {
+        return _kD;
+    }
 
-	/**
-	 * Getter of the material's reflection coefficient
-	 * 
-	 * @return reflection coefficient
-	 */
-	public double getKr() {
-		return _kR;
-	}
+    /**
+     * getter of specular attenuation coefficient
+     *
+     * @return specular attenuation coefficient
+     */
+    public double getKs() {
+        return _kS;
+    }
 
-	/**
-	 * Getter of material's reffraction coefficient
-	 * 
-	 * @return reffraction coefficient
-	 */
-	public double getKt() {
-		return _kT;
-	}
+    /**
+     * getter of shininess power
+     *
+     * @return shininess power
+     */
+    public int getShininess() {
+        return _nShininess;
+    }
 
-	/**
-	 * getter of shininess power
-	 *
-	 * @return shininess power
-	 */
-	public int getShininess() {
-		return _nShininess;
-	}
+    /**
+     * getter of reflection coefficient
+     *
+     * @return reflection coefficient
+     */
+    public double getKr() {
+        return _kR;
+    }
 
+    /**
+     * getter of transparency coefficient
+     *
+     * @return transparency coefficient
+     */
+    public double getKt() {
+        return _kT;
+    }
 }
