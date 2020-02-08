744c4ce283904c5373b219026d0346a1a3bf78c0
yoelbassin <46793414+yoelbassin@users.noreply.github.com>
2020-02-08 22:45:14 +0200

    Update RenderTest.java

--- E1/src/unittests/RenderTest.java
+++ E1/src/unittests/RenderTest.java
@@ -19,23 +19,23 @@ public class RenderTest {
         scene.setBackground(new Color(0, 0, 0));
         Geometries geometries = new Geometries();
         scene.setGeometries(geometries);
-        geometries.add(new Sphere(50, new Point3D(0, 0, 150)));
+		geometries.add(new Sphere(new Color(100, 100,100 ),new Material(0,0,0), 50, new Point3D(0, 0, 150)));
 
-        geometries.add(new Triangle( new Point3D( 100, 0, 149),
-                new Point3D(  0, 100, 149),
-                new Point3D( 100, 100, 149)));
+		geometries.add(new Triangle(new Color(100, 100,100 ),new Material(0,0,0), new Point3D( 100, 0, 149),
+				 							new Point3D(  0, 100, 149),
+				 							new Point3D( 100, 100, 149)));
 
-        geometries.add(new Triangle(new Point3D( 100, 0, 149),
-                new Point3D(  0, -100, 149),
-                new Point3D( 100,-100, 149)));
+		geometries.add(new Triangle(new Color(100, 100,100 ),new Material(0,0,0), new Point3D( 100, 0, 149),
+				 			 				new Point3D(  0, -100, 149),
+				 			 				new Point3D( 100,-100, 149)));
 
-        geometries.add(new Triangle(new Point3D(-100, 0, 149),
-                new Point3D(  0, 100, 149),
-                new Point3D(-100, 100, 149)));
+		geometries.add(new Triangle(new Color(100, 100,100 ),new Material(0,0,0), new Point3D(-100, 0, 149),
+				 							new Point3D(  0, 100, 149),
+				 							new Point3D(-100, 100, 149)));
 
-        geometries.add(new Triangle( new Point3D(-100, 0, 149),
-                new Point3D(  0,  -100, 149),
-                new Point3D(-100, -100, 149)));
+		geometries.add(new Triangle(new Color(100, 100,100 ),new Material(0,0,0), new Point3D(-100, 0, 149),
+				 			 				new Point3D(  0,  -100, 149),
+				 			 				new Point3D(-100, -100, 149)));
 
         ImageWriter imageWriter = new ImageWriter("test0", 500, 500, 500, 500);
         Render render = new Render(imageWriter, scene);
